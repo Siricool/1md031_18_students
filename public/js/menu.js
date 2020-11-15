4bd602c5e66695dd34e3b348f48360f91f160483
Siricool <siri.nordlander@hotmail.com>
2020-11-15 13:33:31 +0100

    menu

---
+++ public/js/menu.js
@@ -0,0 +1,22 @@
+
+var burgers = [
+  {
+  "burgerName": "El Maco GRANDE",
+  "lactose": true,
+  "gluten": false,
+  "imgURL": "https://www.mcdonalds.com/content/dam/nordic/nfl/nutrition/Items/Regular/mcd-sv-elmaco-big.jpg"
+},
+{
+  "burgerName": "Deluxe Sandwich",
+  "lactose": true,
+  "gluten": true,
+  "imgURL": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Entree/Entree%20PDP/_0000s_0004_NEWStack6200001CFAPDPDeluxeSandwich1085png.png"
+},
+{
+  "burgerName": "Halloumiburgare",
+  "lactose": false,
+  "gluten": false,
+  "imgURL": "https://www.max.se/contentassets/bd97342be78047eb8f8026452d545538/product_halloumiburgare.png?width=1024&sharpen=5&sigma=1,4&threshold=0"
+}
+
+]
